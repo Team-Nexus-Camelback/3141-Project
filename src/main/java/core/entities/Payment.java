9e58c2ee450b5771e0cf54f5a233799dbe23e5e0
Ryan Philipps <raphilip@mtu.edu>
2017-11-01 15:01:54 -0400

    Fleshed out the Payment Class. Nick look at this for info on how to save payments, removed a redundant class
    Signed-off-by: Ryan Philipps <raphilip@mtu.edu>

--- src/main/java/core/entities/Payment.java
+++ src/main/java/core/entities/Payment.java
@@ -1,9 +1,70 @@
 package core.entities;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
- * Created by ryan on 10/13/17.
- * The class representation of a 'bill', or 'payment' withing the system
- */
+     * Created by Ryan Philipps on 10/13/17.
+     * Date Last Modified: 10/16/17 by Cody Koski
+     * The class representation of a 'bill', or 'payment' within the system
+     */
 public class Payment {
-    // TODO Cody you can start designing this class of other entities
+    private String paymentName;
+    private double amount;
+    private Date dueDate;
+    private int id;
+    private boolean  isPaid;
+
+    /**
+     *
+     * @param paymentName
+     * @param amount
+     * @param dueDate should be in the format of dd-mm-yyyy;
+     */
+    public Payment(int id, String paymentName, double amount, String dueDate) throws ParseException {
+        this.id = id;
+        this.paymentName = paymentName;
+        this.amount = amount;
+        this.dueDate = getDateFromString(dueDate);
+    }
+
+    private Date getDateFromString(String dueDateString) throws ParseException {
+        return DateFormat.getInstance().parse(dueDateString);
+    }
+
+    public int daysTillDueDate(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+        Date today = calendar.getTime();
+        return today.compareTo(dueDate);
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void payPayment(){
+        isPaid = true;
+    }
 }
+
--- src/main/java/core/usecases/GetPayments.java
+++
@@ -1,7 +0,0 @@
-package core.usecases;
-
-/**
- * Created by ryan on 10/31/17.
- */
-public class GetPayments {
-}
