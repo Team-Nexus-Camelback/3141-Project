55d2b71663ba87c9944304ece1581e62ef70cd31
Ryan Philipps <raphilip@mtu.edu>
2017-11-06 09:06:24 -0500

    Fleshed out the models classes
    Signed-off-by: Ryan Philipps <raphilip@mtu.edu>

--- src/main/java/models/Payment.java
+++ src/main/java/models/Payment.java
@@ -1,7 +1,50 @@
 package models;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by ryan on 11/5/17.
  */
 public class Payment {
+    private SimpleStringProperty name;
+    private SimpleDoubleProperty amount;
+    private SimpleObjectProperty<Date> dueDate;
+
+    public Payment(String name, double amount, String dueDate) throws ParseException {
+        this.name = new SimpleStringProperty(name);
+        this.amount = new SimpleDoubleProperty(amount);
+        Date date = DateFormat.getInstance().parse(dueDate);
+        this.dueDate = new SimpleObjectProperty<>(date);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount.get();
+    }
+
+    public SimpleDoubleProperty amountProperty() {
+        return amount;
+    }
+
+    public Date getDueDate() {
+        return dueDate.get();
+    }
+
+    public SimpleObjectProperty<Date> dueDateProperty() {
+        return dueDate;
+    }
 }
