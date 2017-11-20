a0db758a011271be0afa70940af269b70b09296d
Ryan Philipps <raphilip@mtu.edu>
2017-11-20 16:52:48 -0500

    updated some request classes. Fixed errors within the month comparer class
    Signed-off-by: Ryan Philipps <raphilip@mtu.edu>

--- src/main/java/core/Dto/Month/MonthUpdateRequest.java
+++ src/main/java/core/Dto/Month/MonthUpdateRequest.java
@@ -1,7 +1,32 @@
 package core.Dto.Month;
 
+import core.gateways.IRequest;
+
+import java.util.HashMap;
+
 /**
  * Created by ryan on 11/18/17.
  */
-public class MonthUpdateRequest {
+public class MonthUpdateRequest implements IRequest {
+    private double spendingAmount;
+    private HashMap<String, Double> categoryPercents;
+    private String monthDate;
+
+    public MonthUpdateRequest(String monthDate, double spendingAmount, HashMap<String, Double> categoryPercents) {
+        this.spendingAmount = spendingAmount;
+        this.categoryPercents = categoryPercents;
+        this.monthDate = monthDate;
+    }
+
+    public double getSpendingAmount() {
+        return spendingAmount;
+    }
+
+    public HashMap<String, Double> getCategoryPercents() {
+        return categoryPercents;
+    }
+
+    public String getMonthDate() {
+        return monthDate;
+    }
 }
--- src/main/java/core/Dto/Payment/PaymentUpdateRequest.java
+++ src/main/java/core/Dto/Payment/PaymentUpdateRequest.java
@@ -1,7 +1,42 @@
 package core.Dto.Payment;
 
+import core.gateways.IRequest;
+
 /**
  * Created by ryan on 11/18/17.
  */
-public class PaymentUpdateRequest {
+public class PaymentUpdateRequest implements IRequest {
+    private int id;
+    private boolean isPaid;
+    private String date;
+    private String name;
+    private double amount;
+
+    public PaymentUpdateRequest(int id, boolean isPaid, String date, String name, double amount) {
+        this.id = id;
+        this.isPaid = isPaid;
+        this.date = date;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
